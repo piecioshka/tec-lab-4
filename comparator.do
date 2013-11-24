5c673aefc67a6cc38911b5d53532e378740535b6
piecioshka <piecioshka@gmail.com>
2013-11-24 10:25:37 +0100

    indention

--- comparator.do
+++ comparator.do
@@ -4,11 +4,9 @@ add wave -radix unsigned *
 force a 10#10 1
 force b 10#1 1
 run
-
 force a 10#4 1
 force b 10#6 1
 run
-
 force a 10#5 1
 force b 10#5 1
 run
--- cw4_1.v
+++ cw4_1.v
@@ -1,31 +1,30 @@
-    // zadanie na wej?ciówk?
-    // komparator
-    module cw4_1
+// zadanie na wejsciowke
+// komparator
+module cw4_1
     #(
-    parameter N=4
+        parameter N=4
     )
     (
-    input [N-1:0] a,b,
-    output reg altb,aeqb,agtb
+        input [N-1:0] a,b,
+        output reg altb,aeqb,agtb
     );
-    // cia?o funkcji
+
+    // cialo funkcji
     // jezeli a<b to altb=1
     // jezeli a=b to aeqb=1
     // jezeli a>b to agtb=1
-    // intrukcje równoleg?e lub proceduralne
-     
-    // wersja 2. skorzysta? z w?asno?ci:
-    // je?eli ~(A>B) & ~(A=B) to (A<B)
+    // intrukcje rownolegle lub proceduralne
+
+    // wersja 2. skorzystac z wlasnosci:
+    // jezeli ~(A>B) & ~(A=B) to (A<B)
     always@ (*) begin
-  
-      altb = 0;
-      aeqb = 0;
-      agtb = 0;
-    if (a==b)  aeqb = 1'b1;
-    if (a>b)  agtb = 1'b1;
-    if  (!(aeqb || agtb))  altb = 1'b1;
-    
-  end  
-    //porówna? realizacje: RTL Viewer (w Quartus2)
-     
-    endmodule
+        altb = 0;
+        aeqb = 0;
+        agtb = 0;
+        if (a==b)  aeqb = 1'b1;
+        if (a>b)  agtb = 1'b1;
+        if  (!(aeqb || agtb))  altb = 1'b1;
+    end
+    //porownac realizacje: RTL Viewer (w Quartus2)
+
+endmodule
